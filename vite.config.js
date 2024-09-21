cad026a60b86e3686435ed7a3625f4c37ce96471
Zarantuza <leopeyrecosta@gmail.com>
2024-09-21 16:47:56 +0200

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
       },
-      external: ['peerjs']
+      // Remove the external: ['peerjs'] line
     },
   },
   server: {
