88b7e6703b9afcb0d256ef7c38e7b1f7da050e4f
Zarantuza <leopeyrecosta@gmail.com>
2024-09-21 16:46:14 +0200

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/',
+  base: '/multy-game/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
